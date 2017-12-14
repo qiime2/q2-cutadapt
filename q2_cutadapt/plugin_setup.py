9a2937c911493a053cc48b7b2892d2d04f9eff25
Matthew Dillon <matthewrdillon@gmail.com>
2017-12-14 14:57:39 -0700

    SQUASH: citation text

--- q2_cutadapt/plugin_setup.py
+++ q2_cutadapt/plugin_setup.py
@@ -25,6 +25,9 @@ plugin = Plugin(
                 'adapters (e.g. barcodes, primers) in sequence data.',
     short_description='Plugin for removing adapter sequences, primers, and '
                       'other unwanted sequence from sequence data.',
+    citation_text='Martin, M. (2011). Cutadapt removes adapter sequences from '
+                  'high-throughput sequencing reads. EMBnet.Journal, 17(1), '
+                  'pp. 10-12.\ndoi:http://dx.doi.org/10.14806/ej.17.1.200',
 )
 
 plugin.methods.register_function(
