138c89ab1dabb01d1c22ea9a8fe3c4e7b20e5309
Anthony <oddant1@hotmail.com>
2019-07-19 10:21:34 -0700

    SQUASH: Add a new test for mixed orientation

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
             'data/single-end/*',
             'data/paired-end/*',
             'data/paired-end-unordered/*',
+            'data/mixed_orientation/*',
         ],
     },
     zip_safe=False,
