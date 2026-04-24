533b2209a204dbbf11623b4eafdcb777c7570516
Colin Wood <68213641+colinvwood@users.noreply.github.com>
2026-04-24 14:55:47 -0700

    Update q2_cutadapt/plugin_setup.py

--- q2_cutadapt/plugin_setup.py
+++ q2_cutadapt/plugin_setup.py
@@ -161,7 +161,7 @@ plugin.methods.register_function(
             'Trim trailing Poly G tails from 3 prime end. Continues to trim '
             'nucleotides with Phred score quality lower than threshold after '
             'the tail. Note: this should not be used in conjunction with '
-            '`quality_cutoff_3end` as both trim from the 3 prime end.'
+            '`quality_cutoff_3end` as both trim from the 3 prime end. '
             'Passing 0 disables this trim parameter.'
         ),
     },
